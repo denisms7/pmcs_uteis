a1f2f995f6fe7b15cf00eccf4a8998a331d0b184
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2023-05-29 00:34:07 -0300

    inicio migracao de framework

--- paginas/views.py
+++ paginas/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class PaginaInicial(TemplateView):
+    login_url = reverse_lazy('login')
+    template_name = 'paginas\index.html'
