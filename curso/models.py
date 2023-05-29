76f9c689ebe9df623fe8275a4ee35eb272fd1b15
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2023-05-29 20:28:16 -0300

    added

--- curso/models.py
+++ curso/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
-class Video(models.Model):
+class VideoExcel(models.Model):
     title = models.CharField(max_length=100)
-    video_file = models.FileField(upload_to='videos/')
-
+    video_file = models.FileField(upload_to='videos/excel/')
+    
     @property
     def video_url(self):
         return self.video_file.url
+    
+    def __str__(self):
+        return f"{self.title}"
--- curso/views.py
+++ curso/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .models import Video
+from .models import VideoExcel
 
 
 class Cursos(TemplateView):
@@ -8,5 +8,5 @@ class Cursos(TemplateView):
 
 
 def video_player(request, video_id):
-    video = Video.objects.get(pk=video_id)
-    return render(request, 'curso\player.html', {'video': video})
+    Excel = VideoExcel.objects.get(pk=video_id)
+    return render(request, 'curso\player.html', {'video': Excel})
