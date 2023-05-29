91a0b1ef6aa5e17209bdd9f7e486262e97dab7c6
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2023-05-29 08:59:04 -0300

    added hino

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import PaginaInicial, InteligenciaA, PDM
+from .views import PaginaInicial, InteligenciaA, PDM, HinoNacional, HinoEstadual, HinoMunicipal
 
 urlpatterns = [
     path('', PaginaInicial.as_view(), name='inicio'),
-    path('/int-art/', InteligenciaA.as_view(), name='InteligenciaA'),
-    path('/PDM/', PDM.as_view(), name='pdm_municipal'),
+    path('/ia/', InteligenciaA.as_view(), name='InteligenciaA'),
+    path('/pdm/', PDM.as_view(), name='pdm_municipal'),
+
+    path('/hino/nacional/', HinoNacional.as_view(), name='hino_nacional'),
+    path('/hino/estadual/', HinoEstadual.as_view(), name='hino_estadual'),
+    path('/hino/municipal/', HinoMunicipal.as_view(), name='hino_municipal'),
 ]
