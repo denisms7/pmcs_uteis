6de7367f46e6a321f9a92c734a083e0595cb4f65
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2023-06-09 19:58:52 -0300

    correcao

--- app-uteis/core_django/settings.py
+++ app-uteis/core_django/settings.py
@@ -70,10 +70,15 @@ WSGI_APPLICATION = 'core_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'db'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 
 # Password validation
