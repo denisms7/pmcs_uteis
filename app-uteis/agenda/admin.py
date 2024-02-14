0784f03249367cbdbfe48055a6dc91b218ff2534
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2024-02-14 16:43:10 -0300

    added pesquisa por grupo

--- app-uteis/agenda/admin.py
+++ app-uteis/agenda/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Grupo, Agenda
 
+@admin.register(Grupo)
+class AgendaAdmin(admin.ModelAdmin):
+    list_display = ('grupo', 'numero')
+    search_fields = ['grupo', 'numero']
 
-admin.site.register(Grupo)
-admin.site.register(Agenda)
+@admin.register(Agenda)
+class AgendaAdmin(admin.ModelAdmin):
+    list_display = ('pessoa', 'numero_interno', 'numero_externo')
+    search_fields = ['pessoa', 'numero_interno', 'numero_externo']
