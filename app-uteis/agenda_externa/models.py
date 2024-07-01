02aaaf99af6b876abe2a8bbf1f99fe3912880a6a
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2024-07-01 10:36:30 -0300

    fixed

--- app-uteis/agenda_externa/models.py
+++ app-uteis/agenda_externa/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 class Agenda_Externa(models.Model):
     data = models.DateTimeField(auto_now_add=True, verbose_name=_('Data'))
-    destino = models.CharField(max_length=200, verbose_name=_('Nome'))
+    destino = models.CharField(max_length=200, verbose_name=_('Nome'), unique=True)
     ramal = models.CharField(max_length=4, verbose_name=_('Ramal (Se houver)'), null=True, blank=True)
     numero_externo = models.CharField(max_length=20, verbose_name=_('Número'))
 
