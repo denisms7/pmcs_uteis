161c02c697bc8bc6e2aaf20ac9f7377f7adfece8
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2024-07-01 08:29:44 -0300

    added validação

--- app-uteis/agenda_externa/forms.py
+++ app-uteis/agenda_externa/forms.py
@@ -1,5 +1,5 @@
-
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Agenda_Externa
 
 class FormAgendaExterna(forms.ModelForm):
@@ -12,4 +12,16 @@ class FormAgendaExterna(forms.ModelForm):
             'numero_externo',
         ]
 
+    def clean_numero_externo(self):
+        numero = self.cleaned_data.get('numero_externo')
+        if len(numero) < 14 or len(numero) > 16:
+            raise ValidationError('Número inválido')
+        return numero
 
+
+    def clean_ramal(self):
+        ramal = self.cleaned_data.get('ramal')
+        if ramal is not None and len(ramal) > 0:
+            if not ramal.isdigit():
+                raise ValidationError('O ramal deve conter apenas números')
+        return ramal
