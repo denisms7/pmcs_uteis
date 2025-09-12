427ba99caa4415b7e9f37ce8ed1e4a11c4d611ee
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2025-09-12 14:55:01 -0300

    added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
+from django.utils import timezone
+from datetime import timedelta
 from django.views.generic import TemplateView
 from generic.models import Category, Button, LegislationButton
+from curso.models import Video
 
+def has_recent_video():
+    ten_days_ago = timezone.now() - timedelta(days=10)
+    # Verifica se existe algum vídeo com data maior ou igual a 10 dias atrás
+    return Video.objects.filter(data__gte=ten_days_ago).exists()
 
 
 class HomeTemplateView(TemplateView):
@@ -11,6 +18,7 @@ class HomeTemplateView(TemplateView):
         context['category'] = Category.objects.filter(active=True)
         context['legislation'] = LegislationButton.objects.filter(active=True)
         context['button'] = Button.objects.filter()
+        context['has_recent_video'] = has_recent_video()
         return context
 
 
