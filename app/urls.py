cad7a93b43ff08b402fca8f7a1589e4d0ec97926
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2025-09-12 17:06:37 -0300

    added sair

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from .views import HomeTemplateView, IaTemplateView
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -8,6 +9,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', HomeTemplateView.as_view(), name='home'),
     path('ia/', IaTemplateView.as_view(), name='ia'),
 
