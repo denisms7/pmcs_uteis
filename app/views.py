321c3f971995ab6dfad728fe446f55fc0f39ffa0
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2025-09-13 01:12:58 -0300

    added

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.utils import timezone
+from datetime import date
 from datetime import timedelta
 from django.views.generic import TemplateView
 from generic.models import Category, Button, LegislationButton
 from curso.models import Video
+from events.models import Birthday
 
 def has_recent_video():
     ten_days_ago = timezone.now() - timedelta(days=10)
@@ -12,9 +14,11 @@ def has_recent_video():
 
 class HomeTemplateView(TemplateView):
     template_name = 'home/home.html'
+    
     def get_context_data(self, **kwargs):
+        today = date.today()
         context = super().get_context_data(**kwargs)
-        context['birthday'] = None
+        context['birthday'] = Birthday.objects.filter(birth__month=today.month, birth__day=today.day)
         context['category'] = Category.objects.filter(active=True)
         context['legislation'] = LegislationButton.objects.filter(active=True)
         context['button'] = Button.objects.filter()
